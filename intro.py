aeb194f96f964a226a1ca431f77cf48f8047dc96
Anny Gutierrez <annyggw@gmail.com>
2024-07-10 18:13:39 -0600

    Commit2 Adding Questions and Score

--- intro.py
+++ intro.py
@@ -4,7 +4,42 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play!")
+score = 0
+
+#1
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect! ")
+
+#2
+answer = input("What does GPU stand for?  ")
+if answer.lower() == "graphic processing unit":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect! ")
+
+#3
+answer = input("What does RAM stand for?   ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect! ")
+
+# 4
+answer = input("What does PSU stand for?   ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect! ")
+
+print("Score: " + str(score) + "questions correct.")
